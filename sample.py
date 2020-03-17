decdb218f1ec693ea254899bf6fc25130f94b5fd
Xenia <lommy101@gmail.com>
2020-03-17 14:02:46 +0900

    set views

--- sample.py
+++ sample.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def home():
-    return "Hello"
+    return render_template("index.html")
+
+@app.route('/GetData', methods = ['POST', 'GET'])
+def GetData():
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+
+        
+
+        flash('Good', 'success') 
+        return redirect(url_for('home'))
+    
+    return render_template("index.html")
  
 if __name__ == "__main__":              
-    app.run(host="localhost", port=5000)
+    app.run(host="localhost", port=5000, use_reloader=False, debug=True)
