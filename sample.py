72d0548d89f1a614e1e4c3a8c36aa9611ea3fad9
Xenia <lommy101@gmail.com>
2020-03-18 11:38:42 +0900

    add csv rendering

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -12,13 +13,16 @@ def GetData():
     if request.method == 'POST':
         data = request.form
         print(data)
-
+        df = RenderCSV()
         
-
-        flash('Good', 'success') 
-        return redirect(url_for('home'))
+        flash('Good', 'success')
+        return render_template("index.html", tables=[df.to_html(classes='data')], titles=df.columns.values)
     
     return render_template("index.html")
- 
+
+def RenderCSV():
+    df = pd.read_csv('./static/output/output.csv', encoding= 'euc-kr')
+    return df
+
 if __name__ == "__main__":              
     app.run(host="localhost", port=5000, use_reloader=False, debug=True)
